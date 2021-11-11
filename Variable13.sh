9e45880d57bed84f4342273dc035519ee825e8a5
oliveeerr <93318707+oliveeerr@users.noreply.github.com>
2021-11-11 11:29:55 +0100

    Add files via upload

---
+++ Variable13.sh
@@ -0,0 +1,13 @@
+echo "escriu lletres separades que contengui alguna h"
+read paraules
+conta=0
+for var in ${paraules[@]}
+do
+    if [ $var = h -o $var = H ]
+    then
+        let conta=$conta+1
+    fi
+done
+echo "Hi ha: $conta h(s)."
+
+
