c506c759546e1be19321960bbc408e177838b33f
oliveeerr <93318707+oliveeerr@users.noreply.github.com>
2021-11-19 09:30:55 +0100

    Add files via upload

---
+++ Potència.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo -n "Base: "
+read base
+echo -n "Potencia: "
+read potencia
+
+res=1
+echo "==> $base^$potencia"
+
+let potencia+=1
+
+while [ $potencia -gt 0 ]
+ do
+ let res=$res*$base
+ let potencia-=1
+ echo "$res"
+done
+
